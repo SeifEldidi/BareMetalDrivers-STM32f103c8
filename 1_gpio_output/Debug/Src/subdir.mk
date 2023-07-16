2642076382fbf6a6258258f2515e8b7104af61d3
Seif Hisham Eldidi <106155115+SeifEldidi@users.noreply.github.com>
2023-07-16 21:24:27 +0300

    Add files via upload

---
+++ 1_gpio_output/Debug/Src/subdir.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/HAL_Gpio.c \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c 
+
+OBJS += \
+./Src/HAL_Gpio.o \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o 
+
+C_DEPS += \
+./Src/HAL_Gpio.d \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -c -I../Inc -I"H:/EmbeddedSystems/STMF103/Chip_Headers/CMSIS/Device/ST/STM32F1xx/Include" -I"H:/EmbeddedSystems/STMF103/Chip_Headers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
---
+++ 1_gpio_output/Inc/Std_types.h
@@ -0,0 +1,39 @@
+/*
+ * Std_types.h
+ *
+ *  Created on: Jul 16, 2023
+ *      Author: Seif pc
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+/*---------Includes Section Start----------*/
+#include <stdint.h>
+#include "stm32f103xb.h"
+/*---------Includes Section End------------*/
+
+/*---------Macro Declarations Start----------*/
+#define BIT_MASK 0x01U
+#define NULL (void *)0
+#define CLEAR_MASK_GPIO 0x00000000FUL
+/*---------Macro Declarations End------------*/
+
+/*---------Function Macro Declarations Start---------*/
+#define SET_BIT(REG,BIT_POS) 	(REG|=(BIT_MASK<<BIT_POS))
+#define CLEAR_BIT(REG,BIT_POS)	(REG&=(~(BIT_MASK<<BIT_POS)))
+#define TOGGLE_BIT(REG,BIT_POS) (REG^=(BIT_MASK<<BIT_POS))
+/*---------Function Macro Declarations End-----------*/
+
+/*---------Data Types Section Declaration Start------*/
+
+/*---------Data Types Section Declaration End--------*/
+
+/*---------Software Interfaces Declaration Start-------*/
+
+
+
+/*---------Software Interfaces Declaration End-------*/
+
+
+#endif /* STD_TYPES_H_ */
