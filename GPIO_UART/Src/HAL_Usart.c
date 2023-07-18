5810afc10d700c55daeca05baaa276108ec9b229
Seif Hisham Eldidi <106155115+SeifEldidi@users.noreply.github.com>
2023-07-18 21:08:53 +0300

    Update HAL_Usart.c

--- GPIO_UART/Src/HAL_Usart.c
+++ GPIO_UART/Src/HAL_Usart.c
@@ -11,6 +11,151 @@ static uint16_t Baud_Rate_Gen(uint32_t PERIPH_CLK,uint32_t BAUD_rate)
 	return ((PERIPH_CLK + (BAUD_rate / 2)) / BAUD_rate);
 }
 
+static uint32_t PLL_CLK_CALC_S(uint32_t CLK)
+{
+	switch((RCC->CFGR & ((0x0000000F) << 18))>>18)
+	{
+		case 0x00000000U:
+			CLK*=2;
+			break;
+		case 0x00000001U:
+			CLK*=3;
+			break;
+		case 0x00000002U:
+			CLK*=4;
+			break;
+		case 0x00000003U:
+			CLK*=5;
+			break;
+		case 0x00000004U:
+			CLK*=6;
+			break;
+		case 0x00000005U:
+			CLK*=7;
+			break;
+		case 0x00000006U:
+			CLK*=8;
+			break;
+		case 0x00000007U:
+			CLK*=9;
+			break;
+		case 0x00000008U:
+			CLK*=10;
+			break;
+		case 0x00000009U:
+			CLK*=11;
+			break;
+		case 0x0000000AU:
+			CLK*=12;
+			break;
+		case 0x0000000BU:
+			CLK*=13;
+			break;
+		case 0x0000000CU:
+			CLK*=14;
+			break;
+		case 0x0000000DU:
+			CLK*=15;
+			break;
+		case 0x0000000EU:
+			CLK*=16;
+			break;
+		case 0x0000000FU:
+			CLK*=17;
+			break;
+	}
+	return CLK;
+}
+
+static uint32_t PLL_CLK_CALC()
+{
+	uint32_t CLK=8000000UL;
+	switch((RCC->CFGR & ((0x00000001) << 16))>>16)
+	{
+		case 0:
+			CLK/=2;
+			CLK=PLL_CLK_CALC_S(CLK);
+			break;
+		case 1:
+			switch((RCC->CFGR & ((0x00000001) << 17))>>17)
+			{
+				case 0:
+					CLK=PLL_CLK_CALC_S(CLK);
+					break;
+				case 1:
+					CLK/=2;
+					CLK=PLL_CLK_CALC_S(CLK);
+					break;
+			}
+			break;
+	}
+	return CLK;
+}
+
+
+
+static uint32_t CLK_CALC(uint32_t CLK,uint8_t SH)
+{
+	uint32_t CLK_RET = CLK;
+	switch ((RCC->CFGR & ((0x0000000F) << 4))>>4) {
+		case 0b0000:
+		case 0b0001:
+		case 0b0010:
+		case 0b0011:
+		case 0b0100:
+		case 0b0101:
+		case 0b0110:
+		case 0b0111:
+			CLK_RET /= 1;
+			break;
+		case 0b1000:
+			CLK_RET /= 2;
+			break;
+		case 0b1001:
+			CLK_RET /= 4;
+			break;
+		case 0b1010:
+			CLK_RET /= 8;
+			break;
+		case 0b1011:
+			CLK_RET /= 16;
+			break;
+		case 0b1100:
+			CLK_RET /= 64;
+			break;
+		case 0b1101:
+			CLK_RET /= 128;
+			break;
+		case 0b1110:
+			CLK_RET /= 256;
+			break;
+		case 0b1111:
+			CLK_RET /= 512;
+			break;
+	}
+	switch ((RCC->CFGR & (0x00000007 << SH))>>SH) {
+		case 0b000:
+		case 0b001:
+		case 0b010:
+		case 0b011:
+			CLK_RET /=1;
+			break;
+		case 0b100:
+			CLK_RET /= 2;
+			break;
+		case 0b101:
+			CLK_RET /= 4;
+			break;
+		case 0b110:
+			CLK_RET /= 8;
+			break;
+		case 0b111:
+			CLK_RET /= 16;
+			break;
+	}
+	return CLK_RET;
+}
+
 void HAL_UART_Init(UART_t *HAL_Uart)
 {
 	if(HAL_Uart!=NULL)
@@ -103,8 +248,21 @@ void HAL_UART_Init(UART_t *HAL_Uart)
 				HAL_USART_CONFIG->CR1 &= HAL_USART_PARITY_EVEN;
 				break;
 		}
-		/*Set Baud Rate of the USART Module should first calculate peripheral frequency which will be implemented later on*/
-		HAL_USART_CONFIG->BRR=Baud_Rate_Gen(SYS_FREQ,HAL_Uart->BaudRate);
+		uint8_t SH_M=(HAL_Uart->Instance==USART1)?11:8;
+		switch((RCC->CFGR & (0x00000003<<2))>>2)
+		{
+			case 0x00000000UL:/*----HSI----*/
+				USART_PERIPH_CLK=CLK_CALC(8000000UL,SH_M);
+				break;
+			case 0x00000001UL:/*---HSR----*/
+				USART_PERIPH_CLK=CLK_CALC(8000000UL,SH_M);
+				break;
+			case 0x00000002UL:/*---PLL----*/
+				USART_PERIPH_CLK=PLL_CLK_CALC();
+				USART_PERIPH_CLK=CLK_CALC(USART_PERIPH_CLK,SH_M);
+				break;
+		}
+		HAL_USART_CONFIG->BRR=Baud_Rate_Gen(USART_PERIPH_CLK,HAL_Uart->BaudRate);
 		HAL_USART_CONFIG->CR1|=HAL_USART_UEEN_MSK;
 	}
 }
