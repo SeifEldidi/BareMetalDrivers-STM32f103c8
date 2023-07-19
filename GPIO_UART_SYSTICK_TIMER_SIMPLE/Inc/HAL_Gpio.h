45e66a905b2d8c9e5942b5c83342b2d73aaae0f2
Seif Hisham Eldidi <106155115+SeifEldidi@users.noreply.github.com>
2023-07-19 22:58:37 +0300

    Update HAL_Gpio.h

--- GPIO_UART_SYSTICK_TIMER_SIMPLE/Inc/HAL_Gpio.h
+++ GPIO_UART_SYSTICK_TIMER_SIMPLE/Inc/HAL_Gpio.h
@@ -14,6 +14,26 @@
 /*---------Includes Section End------------*/
 
 /*---------Macro Declarations Start----------*/
+#define HAL_GPIO_INT0_EN EN
+#define HAL_GPIO_INT1_EN DIS
+#define HAL_GPIO_INT2_EN DIS
+#define HAL_GPIO_INT3_EN DIS
+#define HAL_GPIO_INT4_EN DIS
+#define HAL_GPIO_INT5_EN DIS
+#define HAL_GPIO_INT6_EN DIS
+#define HAL_GPIO_INT7_EN DIS
+#define HAL_GPIO_INT8_EN DIS
+#define HAL_GPIO_INT9_EN DIS
+#define HAL_GPIO_INT10_EN DIS
+#define HAL_GPIO_INT11_EN DIS
+#define HAL_GPIO_INT12_EN DIS
+#define HAL_GPIO_INT13_EN DIS
+#define HAL_GPIO_INT14_EN DIS
+#define HAL_GPIO_INT15_EN DIS
+
+#define HAL_GPIO_INT_FLAG_EN EN
+#define HAL_GPIO_INT_FLAG_DIS DIS
+
 #define HAL_GPIO_OUT_PP				0x00000001UL
 #define HAL_GPIO_OUT_OD				0x00000002UL
 #define HAL_GPIO_OUT_ALT_PP			0x00000003UL
@@ -40,6 +60,9 @@
 #define HAL_GPIO_SPEED_FREQ_MEDIUM 	0x00000001UL
 #define HAL_GPIO_SPEED_FREQ_HIGH   	0x00000003UL
 
+#define HAL_GPIO_INT_REDG		 0U
+#define HAL_GPIO_INT_FEDG		 1U
+
 #define GPIO_PIN_0				0U
 #define GPIO_PIN_1				1U
 #define GPIO_PIN_2				2U
@@ -56,6 +79,48 @@
 #define GPIO_PIN_13				13U
 #define GPIO_PIN_14				14U
 #define GPIO_PIN_15				15U
+
+#define EXTI_LINE_0				0U
+#define EXTI_LINE_1				1U
+#define EXTI_LINE_2				2U
+#define EXTI_LINE_3				3U
+#define EXTI_LINE_4				4U
+#define EXTI_LINE_5				5U
+#define EXTI_LINE_6				6U
+#define EXTI_LINE_7				7U
+#define EXTI_LINE_8				8U
+#define EXTI_LINE_9				9U
+#define EXTI_LINE_10			10U
+#define EXTI_LINE_11			11U
+#define EXTI_LINE_12			12U
+#define EXTI_LINE_13			13U
+#define EXTI_LINE_14			14U
+#define EXTI_LINE_15			15U
+#define EXTI_LINE_16			16U
+#define EXTI_LINE_17			17U
+#define EXTI_LINE_18			18U
+#define EXTI_LINE_19			19U
+
+#define EXTI_LINE_0_EN				(1<<EXTI_LINE_0)
+#define EXTI_LINE_1_EN				(1<<EXTI_LINE_1)
+#define EXTI_LINE_2_EN				(1<<EXTI_LINE_2)
+#define EXTI_LINE_3_EN				(1<<EXTI_LINE_3)
+#define EXTI_LINE_4_EN				(1<<EXTI_LINE_4)
+#define EXTI_LINE_5_EN				(1<<EXTI_LINE_5)
+#define EXTI_LINE_6_EN				(1<<EXTI_LINE_6)
+#define EXTI_LINE_7_EN				(1<<EXTI_LINE_7)
+#define EXTI_LINE_8_EN				(1<<EXTI_LINE_8)
+#define EXTI_LINE_9_EN				(1<<EXTI_LINE_9)
+#define EXTI_LINE_10_EN				(1<<EXTI_LINE_10)
+#define EXTI_LINE_11_EN				(1<<EXTI_LINE_11)
+#define EXTI_LINE_12_EN				(1<<EXTI_LINE_12)
+#define EXTI_LINE_13_EN				(1<<EXTI_LINE_13)
+#define EXTI_LINE_14_EN				(1<<EXTI_LINE_14)
+#define EXTI_LINE_15_EN				(1<<EXTI_LINE_15)
+#define EXTI_LINE_16_EN				(1<<EXTI_LINE_16)
+#define EXTI_LINE_17_EN				(1<<EXTI_LINE_17)
+#define EXTI_LINE_18_EN				(1<<EXTI_LINE_18)
+#define EXTI_LINE_19_EN				(1<<EXTI_LINE_19)
 /*---------Macro Declarations End------------*/
 
 /*---------Function Macro Declarations Start---------*/
@@ -65,13 +130,13 @@
 #define __HAL_RCC_GPIOD_EN() (SET_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPDEN_Pos))
 #define __HAL_RCC_GPIOE_EN() (SET_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPEEN_Pos))
 
+#define __HAL_RCC_AFIO_EN() (SET_BIT(RCC->APB2ENR,RCC_APB2ENR_AFIOEN_Pos))
+
 #define __HAL_RCC_GPIOA_D()  (CLEAR_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPAEN_Pos))
 #define __HAL_RCC_GPIOB_D()  (CLEAR_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPBEN_Pos))
 #define __HAL_RCC_GPIOC_D()  (CLEAR_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPCEN_Pos))
 #define __HAL_RCC_GPIOD_D()  (CLEAR_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPDEN_Pos))
 #define __HAL_RCC_GPIOE_D()  (CLEAR_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPEEN_Pos))
-#define __HAL_RCC_GPIOF_D()  (CLEAR_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPFEN_Pos))
-#define __HAL_RCC_GPIOG_D()  (CLEAR_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPFGN_Pos))
 /*---------Function Macro Declarations End-----------*/
 
 /*---------Data Types Section Declaration Start------*/
@@ -81,6 +146,15 @@ typedef struct
 	uint32_t SPEED;
 	uint32_t PIN;
 	uint32_t PULL;
+	uint8_t EXT_N;
+#if (HAL_GPIO_INT0_EN==EN)||(HAL_GPIO_INT1_EN==EN)||(HAL_GPIO_INT2_EN==EN)||(HAL_GPIO_INT3_EN==EN)\
+	||(HAL_GPIO_INT4_EN==EN)||(HAL_GPIO_INT5_EN==EN)||(HAL_GPIO_INT6_EN==EN)||(HAL_GPIO_INT7_EN==EN)\
+	||(HAL_GPIO_INT8_EN==EN)||(HAL_GPIO_INT9_EN==EN)||(HAL_GPIO_INT10_EN==EN)||(HAL_GPIO_INT11_EN==EN)\
+	||(HAL_GPIO_INT12_EN==EN)||(HAL_GPIO_INT13_EN==EN)||(HAL_GPIO_INT14_EN==EN)||(HAL_GPIO_INT15_EN==EN)
+	void (*EXTI_CLL) (void);
+	uint16_t EXTI_LINE;
+	uint8_t  EDGE_TRIGGER;
+#endif
 }GPIO_t;
 
 typedef enum
